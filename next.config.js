efa62126c8a2acadf5f5e2b2676ab8b283101090
victor zarjevski <zarzevskivictor@gmail.com>
2026-07-05 21:19:01 +0300

    fix build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['bcrypt'],
+  serverExternalPackages: ['bcrypt', 'ccxt'],
   images: {
     remotePatterns: [
       {
